6839a2f0845ca433f1cc78871349492e37d4af90
April <aprilyanli38@gmail.com>
2020-12-15 01:11:45 -0600

    tweaked blogroll again

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -9,10 +9,10 @@ class BlogRoll extends React.Component {
     const { edges: posts } = data.allMarkdownRemark
 
     return (
-      <div className="columns is-multiline is-centered">
+      <div className="columns is-centered is-multiline">
         {posts &&
           posts.map(({ node: post }) => (
-            <div className="is-parent column is-8 is-centered" key={post.id}>
+            <div className="is-parent column is-8" key={post.id}>
               <article
                 className={`blog-list-item tile is-child box notification ${
                   post.frontmatter.featuredpost ? 'is-featured' : ''
